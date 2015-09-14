4696c37fe0fe0c2eb918274d6726592e99bcf23d
Matthias Kentzia <k3ntz@users.noreply.github.com>
2015-09-14 13:10:29 +0200

    Added some basic functionality and tests

--- js/app.js
+++ js/app.js
@@ -3,7 +3,22 @@ var testingAngularApp = angular.module('testingAngularApp', []);
 testingAngularApp.controller('TestingAngularCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
     $scope.title = "Testing AngularJS Applications";
     
-    $scope.phones = ['phone1', 'phone2', 'phone3'];
+    $scope.cities = [];
+    $scope.newCity = {};
     
+    $scope.addDestination = function (isValid) {
+        
+        if(isValid) {
+            $scope.cities.push(
+            { 
+                name: $scope.newCity.name, 
+                duration: $scope.newCity.duration
+            });
+        }
+    }
+    
+    $scope.removeCityByIndex = function (index) {
+        $scope.cities.splice(index,1);
+    }
   
 }]);
