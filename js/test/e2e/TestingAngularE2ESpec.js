1f017a916e3d41bc11a1a3077b31a8d5b4923669
Matthias Kentzia <k3ntz@users.noreply.github.com>
2015-09-21 10:56:52 +0200

    Added some protractor tests

--- js/test/e2e/TestingAngularE2ESpec.js
+++ js/test/e2e/TestingAngularE2ESpec.js
@@ -2,6 +2,20 @@ describe('TestingAngularApp', function() {
 
     describe('TestingAngular List view', function() {
 
+        //NOTE: First define these variables in the test, with multiple tests define at the top
+        var newCityField = element(by.model('newDestination.city'));
+        var newCountryField = element(by.model('newDestination.country'));
+        var addDestinationButton =  element(by.id('AddDestinationButton'));
+        
+        var destinationList = element.all(by.repeater('destination in destinations'));
+        
+        //NOTE: Helper Functions can be added and reused to write less code
+        var addDestination = function (city, country) {
+            newCityField.sendKeys(city);
+            newCountryField.sendKeys(country);
+            addDestinationButton.click();
+        }
+        
         beforeEach(function() {
             browser.get('http://0.0.0.0:8080/index.html');
         });
@@ -15,7 +29,7 @@ describe('TestingAngularApp', function() {
             var destinationList = element.all(by.repeater('destination in destinations'));
             var query = element(by.model('search'));
 
-            expect(destinationList.count()).toBe(4);
+            expect(destinationList.count()).toBe(5);
 
             query.sendKeys('London');
             expect(destinationList.count()).toBe(1);
@@ -26,10 +40,26 @@ describe('TestingAngularApp', function() {
         }); 
         
         it('should add destination to destination list', function() {
+            
+            var destinationList = element.all(by.repeater('destination in destinations'));
+            expect(destinationList.count()).toBe(5);
+            
+            element(by.model('newDestination.city')).sendKeys("Valletta");
+            element(by.model('newDestination.country')).sendKeys("Malta");
+
+            element(by.id('AddDestinationButton')).click();
+            
+            expect(destinationList.count()).toBe(6);
         
         });
         
         it('should remove destination from destination list', function() {
+            expect(destinationList.count()).toBe(5);
+            
+            //Find last instance of remove destination button and click it
+            element.all(by.name('removeDestinationButton')).last().click();
+            
+            expect(destinationList.count()).toBe(4);
         
         });
         
