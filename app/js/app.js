42a1377bf2a3298888f2ec12c4dee3ef78d517bb
Matthias Kentzia <matthias.kentzia@tipico.com>
2015-11-28 17:00:01 +0100

    Testing XHR Calls with  Backend

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,9 @@
 var testingAngluarApp = angular.module('testingAngularApp', []);
 
-testingAngluarApp.controller('testingAngularCtrl', ['$rootScope', '$scope', function ($rootScope, $scope) {
+testingAngluarApp.controller('testingAngularCtrl', ['$http', '$rootScope', '$scope', function ($http, $rootScope, $scope) {
     
   $scope.title = "Testing AngularJS Applications";
+  $scope.apiKey = "2de143494c0b295cca9337e1e96b00e0";
 
   $scope.destinations = [];
   $scope.newDestination = {
@@ -21,5 +22,24 @@ testingAngluarApp.controller('testingAngularCtrl', ['$rootScope', '$scope', func
   $scope.removeDestination = function (index) {
     $scope.destinations.splice(index, 1);
   }
+  
+  $scope.getWeather = function(destination) {
+    $http.get('http://api.openweathermap.org/data/2.5/weather?q='+ destination.city + "&appid=" + $scope.apiKey)
+      .then(function successCallback(response) {
+        if (response.data.weather) {
+          destination.weather = {};
+          destination.weather.main = response.data.weather[0].main; //return only first weather present
+          destination.weather.temp = $scope.convertKelvinToCelsius(response.data.main.temp); 
+        } else if (response.data.message) {
+          $scope.message = response.data.message;
+        }
+      }, function errorCallback(response) {
+        $scope.message = "Could not retrieve weather for " + destination.city;
+      });
+  };
+    
+  $scope.convertKelvinToCelsius = function(temperature) {
+    return Math.round(temperature - 273);
+  };
     
 }]);
--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -1,17 +1,19 @@
 describe('Testing AngularJS Test Suite', function(){
   
-  var ctrl, rootScope, scope; 
+  var ctrl, httpBackend, rootScope, scope; 
   
   beforeEach(module('testingAngularApp'));
   
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $httpBackend, $rootScope) {
     rootScope = $rootScope;
     scope = rootScope.$new();
     ctrl = $controller('testingAngularCtrl', {$scope:scope});
+    httpBackend = $httpBackend;
   }));
   
   afterEach(function() {
-    //Cleanup code here
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
   });
 
   it('should initialize the scope correctly', function () {
@@ -68,6 +70,35 @@ describe('Testing AngularJS Test Suite', function(){
     expect(scope.destinations[0].city).toBe("New York");
     expect(scope.destinations[0].country).toBe("USA"); 
     expect(scope.destinations[1]).toBeUndefined();
+  });
+  
+  it('should calculate the correct temperature in degrees celsius', function() {
+    var kelvin = 300;
+    var celsius = scope.convertKelvinToCelsius(kelvin);
+
+    expect(celsius).toBe(27);
+  });    
+    
+  it('should update the weather for the specified destination', function() {
+    httpBackend.expectGET("http://api.openweathermap.org/data/2.5/weather?q=London&appid=" + scope.apiKey).respond(
+      {
+        weather: [{main : 'Rain', detail : 'Light rain'}], 
+        main : { temp : 288 }
+      }
+    );
+
+    scope.destination = 
+    {
+            city : "London",
+            country: "England"
+    };
+
+    scope.getWeather(scope.destination);
+
+    httpBackend.flush();
+
+    expect(scope.destination.weather.main).toBe("Rain");
+    expect(scope.destination.weather.temp).toBe(15);
   });
     
 });
