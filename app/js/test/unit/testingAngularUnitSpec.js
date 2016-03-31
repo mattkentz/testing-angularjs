7d12bc1a495018f7f10cf0511a9b951b1c3d0699
Matthias Kentzia <matthias.kentzia@tipico.com>
2016-03-31 10:32:11 +0200

    Before and After

--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -1,16 +1,18 @@
 describe('Testing AngularJS Test Suite', function(){
 
+  beforeEach(module('testingAngularApp'));
+
   describe('Testing AngularJS Controller', function () {
+    beforeEach(inject(function($controller, $rootScope) {
+      scope = $rootScope.$new();
+      ctrl = $controller('testingAngularCtrl', {$scope:scope});
+    }));
+
+    afterEach(function() {
+      
+    });
+
     it('should initialize the title in the scope', function() {
-      module('testingAngularApp');
-
-      var scope = {};
-      var ctrl;
-
-      inject(function($controller) {
-        ctrl = $controller('testingAngularCtrl', {$scope:scope});
-      });
-
       expect(scope.title).toBeDefined();
       expect(scope.title).toBe("Testing AngularJS Applications");
     });
