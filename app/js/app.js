7dd3c917fcecbb00d4080a4a57675a7874a4257d
Matthias Kentzia <matthias.kentzia@tipico.com>
2016-03-31 10:41:04 +0200

    Testing Controllers and Scope Part 1

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,19 @@ var testingAngluarApp = angular.module('testingAngularApp', []);
 
 testingAngluarApp.controller('testingAngularCtrl', function ($rootScope, $scope) {
 
-    $scope.title = "Testing AngularJS Applications";
+  $scope.title = "Testing AngularJS Applications";
 
+  $scope.destinations = [];
+  $scope.newDestination = {
+    city: undefined,
+    country: undefined
+  };
+
+  $scope.addDestination = function () {
+    $scope.destinations.push(
+    {
+      city: $scope.newDestination.city,
+      country: $scope.newDestination.country
+    });
+  };
 });
--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -9,12 +9,37 @@ describe('Testing AngularJS Test Suite', function(){
     }));
 
     afterEach(function() {
-      
+
     });
 
     it('should initialize the title in the scope', function() {
       expect(scope.title).toBeDefined();
       expect(scope.title).toBe("Testing AngularJS Applications");
+    });
+
+    it('should add 2 destinations', function () {
+      expect(scope.destinations).toBeDefined();
+      expect(scope.destinations.length).toBe(0);
+
+      scope.newDestination =
+        {
+          city : "London",
+          country : "England"
+        };
+
+      scope.addDestination();
+
+      expect(scope.destinations.length).toBe(1);
+      expect(scope.destinations[0].city).toBe("London");
+      expect(scope.destinations[0].country).toBe("England");
+
+      scope.newDestination.city = "Frankfurt";
+      scope.newDestination.country = "Germany";
+      scope.addDestination();
+
+      expect(scope.destinations.length).toBe(2);
+      expect(scope.destinations[1].city).toBe("Frankfurt");
+      expect(scope.destinations[1].country).toBe("Germany");
     });
   });
 
